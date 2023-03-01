0e0895f7127d28393556e3cc4859d1025e0793af
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-28 20:45:58 -0500

    Remove exxceptions from the constructors

--- lib/modules/generators/generators/mixto.dart
+++ lib/modules/generators/generators/mixto.dart
@@ -1,5 +1,3 @@
-import 'package:random_generators/validators/validators.dart';
-
 import '../generator.dart';
 
 class Mixto implements Generator {
@@ -9,18 +7,7 @@ class Mixto implements Generator {
   int seed;
 
   Mixto(
-      {required this.a, required this.c, required this.m, required this.seed}) {
-    if (m <= 0) throw Exception("m must be greater than 0");
-    if (a <= 0) throw Exception("a must be greater than 0");
-    if (c <= 0) throw Exception("c must be greater than 0");
-    if (seed <= 0) throw Exception("seed must be greater than 0");
-    if (a >= m) throw Exception("a must be less than m");
-    if (c >= m) throw Exception("c must be less than m");
-    if (seed >= m) throw Exception("seed must be less than m");
-    if (!Validators.isLesserThanArquitecture(m)) {
-      throw Exception("m must be less than 2^32");
-    }
-  }
+      {required this.a, required this.c, required this.m, required this.seed});
 
   @override
   int nextNumber() {
--- lib/modules/generators/generators/multiplicativo.dart
+++ lib/modules/generators/generators/multiplicativo.dart
@@ -1,4 +1,3 @@
-import '../../../validators/validators.dart';
 import '../generator.dart';
 
 class Multiplicativo extends Generator {
@@ -6,16 +5,7 @@ class Multiplicativo extends Generator {
   int m;
   int seed;
 
-  Multiplicativo({required this.a, required this.m, required this.seed}) {
-    if (m <= 0) throw Exception("m must be greater than 0");
-    if (a <= 0) throw Exception("a must be greater than 0");
-    if (seed <= 0) throw Exception("seed must be greater than 0");
-    if (a >= m) throw Exception("a must be less than m");
-    if (seed >= m) throw Exception("seed must be less than m");
-    if (!Validators.isLesserThanArquitecture(m)) {
-      throw Exception("m must be less than 2^32");
-    }
-  }
+  Multiplicativo({required this.a, required this.m, required this.seed});
 
   @override
   int nextNumber() {
