8fc194362849909dceafdc52323486fcd7d92c70
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-03-06 21:53:58 -0500

    🚧 Add generator tester

---
+++ lib/modules/generator_tester_widgets/mean_test_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:random_generators/modules/generator_tester_widgets/abstraction/generator_tester_widget.dart';
+import 'package:random_generators/modules/generator_testers/mean_tester.dart';
+
+class MeanTestWidget extends GeneratorTesterWidget {
+  const MeanTestWidget({super.key, required super.numbers});
+
+  @override
+  State<MeanTestWidget> createState() => _MeanTestWidgetState();
+}
+
+class _MeanTestWidgetState extends State<MeanTestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    var test = MeanTester(numbers: widget.numbers);
+    bool testResult = test.test();
+
+    return Card(
+        color: widget.getColor(testResult),
+        child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Center(
+                    child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(100)),
+                )),
+                Center(
+                  child: Icon(
+                    testResult ? Icons.check : Icons.close,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Expanded(
+            flex: 3,
+            child: Text("Prueba de promedio",
+                style: TextStyle(color: Colors.white, fontSize: 25)),
+          )
+        ]));
+  }
+}
