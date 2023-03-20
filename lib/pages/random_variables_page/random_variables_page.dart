4952387c9fc5eb25171a5761087c6cd8fa04d183
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-03-20 11:50:55 -0500

    🚧 Render the dropdown and wrap it with consumer

--- lib/pages/random_variables_page/random_variables_page.dart
+++ lib/pages/random_variables_page/random_variables_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:random_generators/layout/page_with_sidebar.dart';
+import 'package:random_generators/models/random_variable_state.dart';
+import 'package:random_generators/pages/random_variables_page/components/distribution_dropdown.dart';
 
 class RandomVariablesPage extends StatelessWidget {
   const RandomVariablesPage({super.key});
+
+  onDropdownSelected(int? value) {
+    print(value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +17,19 @@ class RandomVariablesPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Random Variables'),
         ),
-        body: const PageWithSidebar(
-            sidebarChild: Placeholder(), child: Placeholder()));
+        body: Consumer<RandomVariableState>(
+          builder: (context, value, child) {
+            var entries = value.distributionNames;
+
+            return PageWithSidebar(
+                sidebarChild: Column(
+                  children: [
+                    DistributionDropdown(
+                        entries: entries, onSelected: onDropdownSelected)
+                  ],
+                ),
+                child: const Placeholder());
+          },
+        ));
   }
 }
