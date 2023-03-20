1c09d0526226702b533454f88b110e3edb0a6f0e
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-03-20 18:40:26 -0500

    🚧 Create the distribution form template widget

---
+++ lib/pages/random_variables_page/components/distribution_forms/distribution_form.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+abstract class DistributionForm extends StatefulWidget {
+  const DistributionForm({super.key, required this.onSubmit});
+
+  List<Widget> get formFields;
+  final Function() onSubmit;
+
+  @override
+  State<DistributionForm> createState() => _DistributionFormState();
+}
+
+class _DistributionFormState extends State<DistributionForm> {
+  final GlobalKey<FormState> _formState = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Form(
+              key: _formState,
+              child: ListView(
+                children: [
+                  Wrap(
+                    children: [
+                      for (var field in widget.formFields) field,
+                    ],
+                  )
+                ],
+              )),
+        ),
+        ElevatedButton(
+            onPressed: widget.onSubmit(),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: const ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(0))),
+                minimumSize: const Size.fromHeight(50)),
+            child: Row(
+              children: const [
+                Icon(Icons.search, size: 20),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("Obtener variables aleatorias")
+              ],
+            )),
+      ],
+    );
+  }
+}
