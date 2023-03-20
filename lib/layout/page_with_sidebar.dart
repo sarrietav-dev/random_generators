3b135f3945e698d94a67acaf7bd27f918efe9915
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-03-20 11:26:48 -0500

    💄 Add a page with sidebar layout

---
+++ lib/layout/page_with_sidebar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PageWithSidebar extends StatelessWidget {
+  const PageWithSidebar(
+      {super.key, required this.sidebarChild, required this.child});
+
+  final Widget sidebarChild;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border(
+              right: BorderSide(
+                color: Colors.grey.shade300,
+                width: 1,
+              ),
+            ),
+          ),
+          child: SizedBox(width: 300, child: sidebarChild),
+        ),
+        child
+      ],
+    );
+  }
+}
