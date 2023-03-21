ec4c2ba07c1e8774b8e58841fe3d752c069bb68d
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-03-20 22:32:57 -0500

    ♻️ Add extra column to the number list

--- lib/components/number_list.dart
+++ lib/components/number_list.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
 class NumberList<T> extends StatelessWidget {
-  const NumberList({super.key, required this.numbers});
+  const NumberList(
+      {super.key, required this.numbers, this.extraColumns, this.extraRows})
+      : assert(extraColumns == null || extraColumns.length == numbers.length),
+        assert(extraRows == null || extraRows.length == numbers.length);
 
   final List<T> numbers;
+  final List<DataColumn>? extraColumns;
+  final List<DataRow>? extraRows;
 
   @override
   Widget build(BuildContext context) {
     return DataTable(
-      columns: const [
-        DataColumn(label: Text("Number"), tooltip: "Random numbers generated"),
+      columns: [
+        const DataColumn(
+            label: Text("Numers aleatorios"),
+            tooltip: "Numeros aleatorios generados"),
+        ...extraColumns ?? []
       ],
-      rows: numbers
-          .map(
-            (number) => DataRow(
-              cells: [
-                DataCell(
-                  Text(number.toString()),
-                )
-              ],
-            ),
-          )
-          .toList(),
+      rows: [
+        ...numbers
+            .map(
+              (number) => DataRow(
+                cells: [
+                  DataCell(
+                    Text(number.toString()),
+                  )
+                ],
+              ),
+            )
+            .toList(),
+        ...extraRows ?? []
+      ],
     );
   }
 }
